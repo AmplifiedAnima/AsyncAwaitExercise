902ffbfaff86ffa4d670a93c55a475e59f42fc69
AmplifiedAnima <czupamichal@gmail.com>
2023-01-27 15:13:05 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,61 @@
+
+
+function appendData(data) {
+    const tbl = document.createElement("table");
+    const tblBody = document.createElement("tbody");
+
+    for (let i = 0; i < data.length; i++) {
+        // table data and table row creation
+        const row = document.createElement("tr");
+        const nameOfChad = document.createElement("td");
+        const companyOfChad = document.createElement("td");
+        const idOfChad = document.createElement('td');
+        // appending data from Json
+        const cellText = document.createTextNode(data[i].fullName);
+        const cellText2 = document.createTextNode(data[i].company);
+        const cellText3 = document.createTextNode(data[i].id);
+
+        nameOfChad.appendChild(cellText);
+        companyOfChad.appendChild(cellText2);  
+        idOfChad.appendChild(cellText3);
+
+        row.appendChild(idOfChad);
+        row.appendChild(nameOfChad);
+        row.appendChild(companyOfChad);
+
+        tblBody.appendChild(row);
+
+      }
+      tbl.appendChild(tblBody);
+      document.body.appendChild(tbl);
+      tbl.setAttribute("border", "2");
+    }
+
+
+
+const asyncForPete = async () => {
+    
+    try {
+       
+        const res = await fetch('./db.json');
+        const data = await res.json();
+        appendData(data);
+    }
+    catch(error){
+        console.log(error)
+    }
+    finally{
+        console.log('data has been fetched - like a chad!')
+    }
+}
+
+
+function resAfter2Sec() {
+    return new Promise( resolve => {
+        setTimeout( () => {
+            console.log(resolve);
+            asyncForPete();
+        }, 10000)
+    })
+}
+resAfter2Sec();
